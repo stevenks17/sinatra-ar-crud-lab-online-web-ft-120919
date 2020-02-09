d01f2931277bbcac69e2454e1981687de7019b8d
Steven K <stevenks17@gmail.com>
2020-02-09 17:39:42 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 
+
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,42 +10,51 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    
+    redirect to "/articles"
   end
 
-  get '/posts/new' do
+  # index
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+  # new
+  get "/articles/new" do
+    @article = Article.new
     erb :new
   end
-  
-  post '/posts' do
-    Post.create(params)
-    redirect '/posts'
+
+  # create
+  post "/articles" do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
   end
 
-  get '/posts' do
-    @posts=Post.all
-    erb :inded
+  # show
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    erb :show
   end
 
-  get '/posts/:id' do
-    @post = Post.find(params["id"])
+  # edit
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
     erb :edit
   end
 
-  patch '/posts/:id' do
-    id=params["id"]
-    new params = {}
-    old_post = Post.find(id)
-    new_params[:name] = params["name"]
-    new_params[:content] = params["content"]
-    old_post.update(new_params)
-
-    redirect "posts/#{id}"
+  # update
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
   end
 
-  delete '/posts/:id/delete' do
-    @post = Post.finbd(params["id"])
-    @post.destroy
-    erb :delete
+  #destroy
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
   end
+
+
 end
